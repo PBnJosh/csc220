4311a50c991f818362365c68ec0d8ea357eeeaa8
JLoftus <jloft@illinois.edu>
2026-02-08 13:17:19 -0600

    printed svg canvas and added a function call to print_circles

--- 02-lab/main.py
+++ 02-lab/main.py
@@ -19,6 +19,7 @@ import color
 import point
 from random import randint
 
+# returns a color object with a random rgb value
 def rand_color():
   color = color.Color()
   color.setRed(randInt(0,255))
@@ -26,12 +27,14 @@ def rand_color():
   color.setBlue(randInt(0,255))
   return color
 
+# returns a point object with random x and y coords
 def rand_point():
   point = point.Point()
   point.setAcross(randInt(0,1000))
   point.setDown(randInt(0,1000))
   return point
 
+# prints num_circles circles to the svg canvas
 def print_circles( num_circles ):
   for i in range(0, num_circles):
     color = rand_color()
@@ -39,6 +42,10 @@ def print_circles( num_circles ):
     radius = randInt( 1, 50 )
     circle = circle.Circle( point, radius, color )
     print(circle.SVG())
+
+print ('<svg height="1000" width="1000">')
+print_circles( 1000 )
+print ('</svg>')
 
 # I honor Parkland's core values by affirming that I have 
 # followed all academic integrity guidelines for this work.
