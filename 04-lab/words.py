8537056993976c40dadd3f54ee4e9936ef69de4c
JLoftus <jloft@illinois.edu>
2026-02-13 13:24:02 -0600

    wrote loop to print out 10x10 grid of words and added comments

--- 04-lab/words.py
+++ 04-lab/words.py
@@ -14,24 +14,30 @@ csc220.showForm("This is the comment on the form area.")
 # print ("textbox contains <b>{}</b> <br>".format( textbox ))
 # print ("textarea contains <b>{}</b> <br>".format( textarea ))
 
-# Take in input, split words by " ", make an array of the word lengths, 
-# and calculate the average length
 textarea = csc220.getInput('textarea')
+words = textarea.split()
 
-# words = textarea.split()
-
-# FOR TESTING
-words = ["rhubarb", "bathroom", "apple", "syzygy", "No", "Institution", "miata", "popcorn", 
-         "Lamp", "vaseline", "USB", "bird", "Devil", "Illinois", "water", "pencil", "taxes"]
-
+# Count words, and exit if there's less than 100
 num_words = len(words)
+if num_words < 100:
+    print("<p> Where are all my wonderful words? </p>")
+    exit()
+
+# Calculate average length of words
 word_lengths = [ len(word) for word in words ]
 avg_word_len = statistics.mean(word_lengths)
 
-print(f"There are {num_words} words in this input.")
-print(f"The average length of the words in this input is {avg_word_len}.")
+# Sort words into ascending lexicographical order
+words.sort()
 
+print(f"<p> There are {num_words} words in this input. </p>")
+print(f"<p> The average length of the words in this input is {avg_word_len}. </p>")
+print(words)
 
+# Print first 100 words from sorted list into a 10x10 grid
+for i in range(10):
+    start = i * 10
+    print (f"<p> {words[start : start + 10]} </p>")
 
 
 
