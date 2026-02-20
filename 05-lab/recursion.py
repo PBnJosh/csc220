acb1ac3bd2d4dbc91060cf6c4645c8c0fd0fe67d
JLoftus <jloft@illinois.edu>
2026-02-20 15:57:38 -0600

    did stupid recursion assignments and finished writing 05-lab code

---
+++ 05-lab/recursion.py
@@ -0,0 +1,51 @@
+#!/usr/local/bin/python3
+import sys
+sys.path.append('/home/staff/kurban/python')
+
+import csc220
+
+csc220.showForm("This is the comment on the form area.")  
+
+textarea = csc220.getInput('textarea')
+# textbox = csc220.getInput('textbox')
+
+# print ("<h2>This is at the bottom and can be used for any html output </h2><br>")
+
+# print ("textbox contains <b>{}</b> <br>".format( textbox ))
+# print ("textarea contains <b>{}</b> <br>".format( textarea ))
+
+def path_can_win(pos):
+
+    if (pos == len(board) - 1):
+        path.append(pos)
+        return True
+
+    if ( (pos in visited) or (pos < 0) or (pos >= len(board) ) or (board[pos] == 0) ):
+        return False
+    
+    visited.add(pos)
+
+    if ( path_can_win( pos + board[pos] ) or path_can_win( pos - board[pos] ) ):
+        path.append(pos)
+        return True
+
+    return False
+
+game_vals = textarea.split()
+board = list( int( game_vals[i] ) for i in range( len(game_vals) ) )
+
+path = []
+visited = set()
+
+print(f"Winnable path: { path_can_win(0) }.")
+path.reverse()
+print(f"path: {path}")
+print(f'visited: {visited}')
+print(f'board: {board}')
+
+
+# I honor Parkland's core values by affirming that I have 
+# followed all academic integrity guidelines for this work.
+
+# Josh Loftus
+# there is nothing below here!
