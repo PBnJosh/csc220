05cda803b17062614aa6662ca6d13ebef5b9a3c0
JLoftus <jloft@illinois.edu>
2026-02-27 12:12:01 -0600

    fixed bugs

--- 08-lab/dictionaries.py
+++ 08-lab/dictionaries.py
@@ -4,7 +4,7 @@ sys.path.append('/home/staff/kurban/python')
 
 import csc220
 
-# csc220.showForm("This is the comment on the form area.")  
+csc220.showForm("This is the comment on the form area.")  
 
 textarea = csc220.getInput('textarea')
 # textbox = csc220.getInput('textbox')
@@ -17,15 +17,8 @@ textarea = csc220.getInput('textarea')
 dictionary = {}
 
 # takes a string of the format "x<-6" and adds or modifies the entry in the dictionary
-def store_variable(line):
+def store_variable(vals):
     global dictionary
-    
-    vals = line.split('<-')
-
-    # throw "error" if the syntax is invalid and end program
-    if ( (len(vals) != 2) or ( vals[1] == '') ):
-        print(f'Invalid Syntax: {line}')
-        exit()
         
     # try storing variable as a float
     try:
@@ -37,7 +30,7 @@ def store_variable(line):
             dictionary[ vals[0] ] = dictionary[ vals[1] ]
         except:
             # other variable is not defined
-            print(f'Error: variable { vals[1] } is not defined')
+            print(f'Error: variable { vals[1] } is not defined<br>')
             exit()
 
 # split input into a list of commands
@@ -51,22 +44,23 @@ for i in input_lines:
 
     # process 'NAME' instruction
     if (line == "NAME"):
-        print("Josh Loftus")
+        print("Josh Loftus<br>")
 
     # process 'DUMP' instruction
     elif (line == "DUMP"):
-        print("Printing all variables:")
+        print("Printing all variables:<br>")
         for var in dictionary:
-            print(f'{var} { dictionary[var] }')
+            print(f'{var} { dictionary[var] }<br>')
     
     # process variable assignment instruction
     elif ( line.__contains__('<-') ):
-        store_variable(line)
-    
-    else:
-        print(f"Error: unrecognized syntax: {i}")
-        exit()
-    
+        vals = line.split('<-')
+
+        # throw "error" if the syntax is invalid and end program
+        if ( (len(vals) != 2) or ( vals[1] == '') ):
+            print(f'Invalid Syntax: {line}<br>')
+            exit()
+        store_variable(vals)
 
 
 # I honor Parkland's core values by affirming that I have 
